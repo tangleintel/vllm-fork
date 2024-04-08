2a2a24f2cd764079e74a0353d76ecf15bd40bafd
Sebastian Urwan <surwan@habana.ai>
2024-04-08 13:46:39 +0300

    Added context manager with HPUTimeUtil class

--- time_utils_prototype.py
+++ time_utils_prototype.py
@@ -1,36 +1,23 @@
 import torch
-import habana_frameworks.torch.core as htcore
-import habana_frameworks.torch.gpu_migration
 import asyncio
-import signal
-import multiprocessing
-import pyhlml
-from vllm.hpu.time_utils import device_utilization_counter
-     
+from vllm.utils import is_hpu
+if is_hpu():
+    import habana_frameworks.torch.core as htcore
+    from vllm.hpu.time_utils import HpuTimeUtil
+    import os
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 async def engine_step():
     print("Started engine_step!")
     for i in range(100):
-        x = torch.zeros(100, device=torch.device("hpu"))
-        x += torch.ones(100)
-        await asyncio.sleep(0.01)
-    return "Got req from engine step!"
+        x = torch.zeros((2000,2000), device=torch.device("hpu"))
+        x += torch.ones((2000,2000), device=torch.device("hpu"))
+    return
 
 async def run_engine():
-    device_utilization_queue = multiprocessing.Queue(maxsize=1)
-    device_utilization_results = multiprocessing.Manager().list()
-    device_utilization_process = multiprocessing.Process(
-        target=device_utilization_counter,
-        args=(device_utilization_queue, device_utilization_results)
-    )
-    device_utilization_process.start()    
-
-    engine_step_output = await engine_step()
-    await asyncio.sleep(0)    
-
-    device_utilization_process.kill()
-    device_utilization_process.join()
-    utilization_data = device_utilization_results[-1] if device_utilization_results else None
-    print(f'HPU mean utilization = {utilization_data}')
+    with HpuTimeUtil(interval=0.000005):
+        engine_step_output = await engine_step()
+        await asyncio.sleep(0)
 
 def main():
     asyncio.run(run_engine())
