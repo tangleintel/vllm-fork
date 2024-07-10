ba772030535d4c99931f4b29758afcb8d73cd2fc
Michal Adamczyk <madamczyk@habana.ai>
2024-07-10 13:46:59 +0200

    Use flat block layout for PA (#92)
    * Cleanup AttentionMetadata on HPU
    
    * Flat PA - POC
    
    * Decode warmup overhaul
    
    * Fix input_hash calculation
    
    * Block bucket size 32 -> 16
    
    * Improve host time
    
    * Skip UTs
    
    * Add GQA/MQA
    
    * Add mask instead of filling
    
    * 2d block mapping
    
    * Optional flipping in PA
    
    * Runner updated for 2d block mapping
    
    * Eliminate physical transposes
    
    * POC: build block_bias on device
    
    * Cleanup
    
    * Fix seq_len calculation
    
    * Experimental profiling
    
    * Add missing call to kv_matmul_op
    
    * Fix block_usage calculation
    
    * Change default block bucket step for decode to 128
    
    * Fix max decode block bucket calculation
    
    * Fix block_usage calculations
    
    * Cleanup
    
    * Print values for bucketing vars
    
    * Pass block size do HpuModelAdapter
    
    ---------
    
    Co-authored-by: barak goldberg <149692267+bgoldberg-habana@users.noreply.github.com>

--- vllm/hpu/utils.py
+++ vllm/hpu/utils.py
@@ -47,5 +47,5 @@ class VLLMKVCache(torch.nn.Module):
         insert_or_update_cache(input, cache, num_kv_cache_passes, num_slots_available, block_indices, block_offset)
         return cache
 
-    def fetch_from_cache(self, cache, blocks, permutations):
-        return [cache.index_select(0, blocks[:, i]).permute(permutations) for i in range(blocks.size(1))]
+    def fetch_from_cache(self, cache, blocks):
+        return cache.index_select(0, blocks)
