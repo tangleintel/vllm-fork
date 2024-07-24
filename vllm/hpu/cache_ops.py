e48e8dc19a1a5f60ead8d9b16f87ad43f3da80ce
Michał Kuligowski <michal.kuligowski@intel.com>
2024-07-24 17:26:29 +0200

    Update cache_ops.py
    Replace inplace index_put with out of place version

--- vllm/hpu/cache_ops.py
+++ vllm/hpu/cache_ops.py
@@ -15,7 +15,7 @@ def insert_or_update_cache(input, cache, block_indices, block_offsets):
     if block_offsets is None:
         cache.index_copy_(0, block_indices, input)
     else:
-        cache.index_put_((block_indices, block_offsets), input)
+        cache.index_put((block_indices, block_offsets), input)
 
 
 def swap_blocks(src, dst, block_mapping):
