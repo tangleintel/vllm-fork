088f37259191c2f28e066407e6f410327875c2e3
Konrad Zawora <kzawora@habana.ai>
2024-08-05 19:14:44 +0300

    Revert "typo"
    This reverts commit 5b5dab1371a18783ac274b650fd85d2ad3e0d702.

--- vllm/executor/multiproc_hpu_executor.py
+++ vllm/executor/multiproc_hpu_executor.py
@@ -154,7 +154,7 @@ class MultiprocessingHPUExecutor(DistributedGPUExecutor):
 
         Passing None will cause the driver to stop the model execution
         loop running in each of the remote workers.
-        """
+        """z
         return self.driver_worker.execute_model(execute_model_req)
     
     def _get_create_worker_kwargs(
