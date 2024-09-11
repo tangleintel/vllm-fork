ac3a026acf9430febac4fa64649fe28b0e51da09
Libin Tang <litang@habana.ai>
2024-09-11 19:17:46 +0000

    wa for delayed sampling

--- vllm/sequence.py
+++ vllm/sequence.py
@@ -337,7 +337,7 @@ class Sequence:
         token_id: int,
         logprobs: Dict[int, Logprob],
     ) -> None:
-        assert token_id in logprobs
+        #assert token_id in logprobs
         self.output_logprobs.append(logprobs)
         self.data.append_token_id(
             token_id,
