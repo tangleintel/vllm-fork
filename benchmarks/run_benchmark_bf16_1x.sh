7cbd2c3dfe8ec036024c2a6530db0bada39c6c72
mandy-li <mandy.j.li@intel.com>
2024-09-13 10:08:05 -0700

    modify configs for benchmark

--- benchmarks/run_benchmark_bf16_1x.sh
+++ benchmarks/run_benchmark_bf16_1x.sh
@@ -17,27 +17,27 @@ export VLLM_PROMPT_SEQ_BUCKET_MIN=2048
 export VLLM_PROMPT_SEQ_BUCKET_STEP=2048
 export VLLM_PROMPT_SEQ_BUCKET_MAX=2048
 
-export VLLM_DECODE_BS_BUCKET_MIN=128
-export VLLM_DECODE_BS_BUCKET_STEP=128
-export VLLM_DECODE_BS_BUCKET_MAX=128
+export VLLM_DECODE_BS_BUCKET_MIN=64
+export VLLM_DECODE_BS_BUCKET_STEP=64
+export VLLM_DECODE_BS_BUCKET_MAX=64
 export VLLM_DECODE_SEQ_BUCKET_MIN=2176 #2176
 export VLLM_DECODE_SEQ_BUCKET_STEP=128 #128
 export VLLM_DECODE_SEQ_BUCKET_MAX=4096
 
 export PT_HPUGRAPH_DISABLE_TENSOR_CACHE=true
-
 #PT_HPU_LAZY_MODE=0 
 #
 
 #LOG_LEVEL_ALL=1 HABANA_LOGS=./hlog_393 VLLM_SKIP_WARMUP=true 
-QUANT_CONFIG=/root/npu-stack/vllm-fork/maxabs_quant.json PT_HPU_LAZY_MODE=0 python3 benchmark_throughput.py \
-	--model=/mnt/weka/data/mixtral/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/1e637f2d7cb0a9d6fb1922f305cb784995190a83/ \
+#QUANT_CONFIG=/root/npu-stack/vllm-fork/maxabs_quant.json PT_HPU_LAZY_MODE=0
+python3 benchmark_throughput.py \
+	--model=/root/examples/articul/mixtral_8x7b_instruct/snapshots/1e637f2d7cb0a9d6fb1922f305cb784995190a83 \
 	--input-len=2048 --output-len=2048 \
 	--device=hpu --tensor-parallel-size=1 --dtype bfloat16 \
 	--max-model-len=4096 \
 	--gpu-memory-utilization 0.9 \
 	--num-prompts 1000 \
 	--weights-load-device cpu \
-        --max-num-seqs=128 \
-	--quantization=inc --kv-cache-dtype fp8_inc
+  --max-num-seqs=64
+
 	
