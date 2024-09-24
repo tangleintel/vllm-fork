541b737207860887d25bc2681b62ed39b213fac3
Libin Tang <litang@habana.ai>
2024-09-23 22:47:55 -0700

    Pad the meta data with dummpy seq data function

--- vllm/worker/habana_model_runner.py
+++ vllm/worker/habana_model_runner.py
@@ -1099,9 +1099,11 @@ class HabanaModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
         batch_size_padding = batch_size_padded - real_batch_size
         seq_group_metadata_list = seq_group_metadata_list.copy()
 
-        #seq_group_metadata_list.extend(seq_group_metadata_list[0]
-        #                               for _ in range(batch_size_padding))
-        seq_group_metadata_list.extend(self.create_dummy_seq_group_metadata(0,0,is_prompt) for _ in range(batch_size_padding))
+        seq_group_metadata_list.extend(seq_group_metadata_list[0]
+                                       for _ in range(batch_size_padding))
+        #if batch_size_padded > 0:
+            #if self.scheduler_config.enable_delayed_sampling and is_prompt:
+                #seq_group_metadata_list.extend(self.create_dummy_seq_group_metadata(0,0,is_prompt) for _ in range(batch_size_padding))
 
         prefill_reqs = []
         decode_reqs = []
