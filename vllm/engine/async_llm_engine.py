4d1afa952396eb8c06ae6e007cfe0388f993af02
Libin Tang <litang@habana.ai>
2024-09-24 16:17:28 -0700

    fix indentation issue

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -692,7 +692,7 @@ class AsyncLLMEngine:
         hb_profer = HabanaProfile(
             warmup=WARMUP_STEP, active=ACTIVE_STEP, record_shapes=False
         )
-        PROFILE=False
+        PROFILE=True
         if PROFILE:
             hb_profer.start()
 
