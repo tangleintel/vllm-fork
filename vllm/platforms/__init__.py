5bb2b20fb6ce0e5e8fb3fc104e633942e82051a2
Harish Subramony <hsubramony@habana.ai>
2024-09-25 14:54:35 +0000

    remove tpu checks

--- vllm/platforms/__init__.py
+++ vllm/platforms/__init__.py
@@ -6,16 +6,6 @@ from .interface import Platform, PlatformEnum, UnspecifiedPlatform
 
 current_platform: Optional[Platform]
 
-is_tpu = False
-try:
-    # While it's technically possible to install libtpu on a non-TPU machine,
-    # this is a very uncommon scenario. Therefore, we assume that libtpu is
-    # installed if and only if the machine has TPUs.
-    import libtpu  # noqa: F401
-    is_tpu = True
-except Exception:
-    pass
-
 is_hpu = False
 try:
     import os
@@ -31,9 +21,6 @@ if torch.version.cuda is not None:
 elif torch.version.hip is not None:
     from .rocm import RocmPlatform
     current_platform = RocmPlatform()
-elif is_tpu:
-    from .tpu import TpuPlatform
-    current_platform = TpuPlatform()
 elif is_hpu:
     from .hpu import HpuPlatform
     current_platform = HpuPlatform()
