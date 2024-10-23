de86a8fb343604bf33223b8c13b7ba90c17366af
Harish Subramony <81822986+hsubramony@users.noreply.github.com>
2024-10-23 02:35:29 -0700

    prevent multiple imports of habana_frameworks (#397)
    when using vllm apis directly and tp>2 , it causes multiple imports of
    habana_frameworks resulting in a crash.
    adding a check to prevent the multiple imports
    
    issue doesnt occur when running command line tests.
    issue only occurs with a diff customer when using script and calling
    vllm apis directly.

--- vllm/_custom_ops.py
+++ vllm/_custom_ops.py
@@ -2,18 +2,17 @@ import contextlib
 import functools
 from typing import List, Optional, Tuple, Type
 import torch
+import importlib.util
 
 from vllm.logger import init_logger
 
 logger = init_logger(__name__)
 
 try:
-    import habana_frameworks.torch.hpu as hthpu
-except ImportError:
-    try:
+    if importlib.util.find_spec("habana_frameworks") is None:
         import vllm._C
-    except ImportError as e:
-        logger.warning("Failed to import from vllm._C with %r", e)
+except ImportError as e:
+    logger.warning("Failed to import from vllm._C with %r", e)
 
 with contextlib.suppress(ImportError):
     import vllm._moe_C
