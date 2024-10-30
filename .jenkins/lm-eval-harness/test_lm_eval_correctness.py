1450e57cc3c2d54ab8bdf912b419a0ab971fd91f
Artur Fierka <afierka@habana.ai>
2024-10-30 11:47:24 +0200

    Add debug print

--- .jenkins/lm-eval-harness/test_lm_eval_correctness.py
+++ .jenkins/lm-eval-harness/test_lm_eval_correctness.py
@@ -57,6 +57,7 @@ def launch_lm_eval(eval_config):
     if eval_config.get("num_scheduled_steps"):
         model_args += \
             f",num_scheduled_steps={eval_config.get('num_scheduled_steps')}"
+        print(f"MODEL_ARGS: {model_args}")
     kwargs = {}
     if 'fewshot_as_multiturn' in eval_config:
         kwargs['fewshot_as_multiturn'] = eval_config['fewshot_as_multiturn']
